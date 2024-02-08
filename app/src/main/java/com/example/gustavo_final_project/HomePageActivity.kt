a945828a5cf118f2e13771031eb3cb0d6bf0096f
gustavoselemem <gustavoselemem@hotmail.com>
2024-02-08 21:02:35 +0000

    Details added to the home page.

--- app/src/main/java/com/example/gustavo_final_project/HomePageActivity.kt
+++ app/src/main/java/com/example/gustavo_final_project/HomePageActivity.kt
@@ -1,33 +1,129 @@
 package com.example.gustavo_final_project
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class HomePageActivity : AppCompatActivity() {
+class HomePageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CreateHomeContent()
+            HomePage()
         }
     }
+}
 
-    @Composable
-    fun CreateHomeContent() {
-        Column {
-            Text(text = "Home Page")
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage() {
+    Scaffold(
+        topBar = { HomeTopBar() },
+        floatingActionButton = { AddEntryButton() },
+        content = { HomeContent() }
+    )
+}
 
-        }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar() {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Default.Create,
+                    contentDescription = null, // describe content if needed
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = "Daily Journal",
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* Handle menu click */ }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+            }
+        },
+    )
+}
+
+@Composable
+fun AddEntryButton() {
+    FloatingActionButton(
+        onClick = { /* Handle button click */ },
+        shape = CircleShape,
+        contentColor = Color.Black
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add"
+        )
     }
+}
 
-    @Preview
-    @Composable
-    fun PreviewCreateHomeContent() {
-        CreateHomeContent()
+@Composable
+fun HomeContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Start journaling",
+            fontSize = 24.sp,
+            color = Color.Black
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Add new entries by tapping on the plus button below.",
+            fontSize = 16.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
     }
+}
 
+@Preview
+@Composable
+fun PreviewHomePage() {
+    HomePage()
 }
